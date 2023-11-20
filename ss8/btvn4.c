1dd4736879fda326554793d6b92af37201858951
hieupham250 <147390672+hieupham250@users.noreply.github.com>
2023-11-20 21:33:39 +0700

    Update btvn4.c

--- ss8/btvn4.c
+++ ss8/btvn4.c
@@ -1,25 +1,48 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-	int n,i,add_Value,add_Index;
-	printf("nhap so phan tu cua mang: ");
+	int addValue,addIndex,n,i;
+	printf("Nhap vao so phan tu cua mang:");
 	scanf("%d",&n);
-	int array[n+1];
+	int oldNumbers[n];
 	for(i=0;i<n;i++){
-		printf("array[%d]= ",i);
-		scanf("%d",&array[i]);
+		printf("oldNumbers[%d]=",i);
+		scanf("%d",&oldNumbers[i]);
 	}
-	printf("nhap phan tu can chen: ");
-	scanf("%d",&add_Value);
-	printf("nhap vi tri can chen: ");
-	scanf("%d",&add_Index);
-	for(i=n;i>=0;i--){
-		array[i] = array[i-1];
-		if(i == add_Index){
-			array[i]=add_Value;
-		}
+	printf("Nhap vao gia tri can chen vao mang:");
+	scanf("%d",&addValue);
+	printf("Nhap vao chi so can chen vao mang:");
+	scanf("%d",&addIndex);
+	printf("Nhap gia tri cac phan tu cua mang:\n");
+	if(addIndex<n){
+		int newNumbers[n+1];
+		for(i=0; i<n+1; i++){
+			if(i<addIndex){
+				newNumbers[i] = oldNumbers[i];
+			}else if(i==addIndex){
+				newNumbers[i] = addValue;
+			}else{
+				newNumbers[i] = oldNumbers[i-1];
+			}		
+		}	
+		printf("Mang sau khi chen phan tu la:\n");
+		for(i=0; i<n+1; i++){
+			printf("%d\t",newNumbers[i]);
+		}		
+	}else{
+		int newNumbers[addIndex+1];
+		for(i=0; i<addIndex; i++){
+			if(i<n){
+				newNumbers[i] = oldNumbers[i];
+			}else{
+				newNumbers[i] = 0;
+			}
+		}			
+		newNumbers[addIndex] = addValue;
+		printf("Mang sau khi chen phan tu la:\n");
+		for(i=0; i<=addIndex; i++){
+			printf("%d\t",newNumbers[i]);
+		}		
 	}
-	for(i=0;i<n;i++){
-		printf("%d\t",array[i]);
-	}
+	
 }
