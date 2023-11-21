ffb4d0aa6aa5ace9c60f5baea786bf02f6869074
hieupham250 <147390672+hieupham250@users.noreply.github.com>
2023-11-21 08:12:04 +0700

    Update btvn5.c

--- ss8/btvn5.c
+++ ss8/btvn5.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n,i,update_Value,update_Index;
+	printf("nhap so phan tu cua mang: ");
+	scanf("%d",&n);
+	int numbers[n];
+	for(i=0;i<n;i++){
+		printf("numbers[%d]=",i);
+		scanf("%d",&numbers[i]);
+	}
+	printf("nhap gia tri can cap nhat: ");
+	scanf("%d",&update_Value);
+	printf("nhap chi so phan tu cap nhat:");
+	scanf("%d",&update_Index);
+	printf("mang sau khi cap nhat:\n");
+	for(i=0;i<n;i++){
+		if(i==update_Index){
+			numbers[i] = update_Value;	
+		}
+		printf("%d\t",numbers[i]);
+	}
+}
 
