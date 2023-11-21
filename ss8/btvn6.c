907035b4ff950834bd9635819dd442e877e765fa
hieupham250 <147390672+hieupham250@users.noreply.github.com>
2023-11-21 07:56:32 +0700

    Update btvn6.c

--- ss8/btvn6.c
+++ ss8/btvn6.c
@@ -1,21 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-	int n,i,delete_Index;
-	printf("nhap so phan tu cua mang: ");
-	scanf("%d",&n);
-	int oldNumbers[n];
-	printf("nhap vao cac gia tri:\n");
-	for(i=0;i<n;i++){
-		printf("numbers[%d]= ",i);
-		scanf("%d",&oldNumbers[i]);
-	}
-	printf("nhap chi so phan tu can xoa: ");
-	scanf("%d",&delete_Index);
-	for(i=delete_Index;i<n-1;i++){
-		oldNumbers[i] = oldNumbers[i + 1];
-	}
-	for(i=0;i<n-1;i++){
-		printf("%d\t",oldNumbers[i]);
-	}
+int main() {
+    int n, i, delete_Index;
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+    int oldNumbers[n];
+    printf("Nhap vao cac gia tri:\n");
+    for (i = 0; i < n; i++) {
+        printf("numbers[%d]= ", i);
+        scanf("%d", &oldNumbers[i]);
+    }
+    printf("Nhap chi so phan tu can xoa: ");
+    scanf("%d", &delete_Index);
+
+    if (delete_Index >= 0 && delete_Index < n) {
+        for (i = delete_Index; i < n - 1; i++) {
+            oldNumbers[i] = oldNumbers[i + 1];
+        }
+        n--;  
+        printf("Mang sau khi xoa:\n");
+        for (i = 0; i < n; i++) {
+            printf("%d\t", oldNumbers[i]);
+        }
+    } else {
+        printf("Chi so phan tu can xoa khong hop le.\n");
+    }
 }
